f7051a1375236337872337778c3eb9efccddcfb3
ronan <ronan_mccabe@protonmail.com>
2025-04-20 17:08:11 -0600

    Bump, add token22 nft test

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use solana_account_decoder::{
     parse_token_extension::{parse_extension, UiExtension, UiTokenMetadata},
 };
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
 use spl_token_2022::{
     extension::{ExtensionType::TokenMetadata, StateWithExtensions},
-    solana_program::pubkey::Pubkey,
     state::Mint,
 };
 
-pub async fn fetch_token_2022_metadata(
+pub async fn fetch_token_2022_nft_metadata(
     rpc: &RpcClient,
     mint: &Pubkey,
 ) -> Result<UiTokenMetadata, Box<dyn std::error::Error>> {
@@ -22,5 +22,20 @@ pub async fn fetch_token_2022_metadata(
         Err(ParseAccountError::AdditionalDataMissing(
             "Metadata not found".to_string(),
         ))?
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_nft_22() {
+        let mainnet_rpc = std::env::var("RPC_URL").unwrap();
+        let client = RpcClient::new(mainnet_rpc);
+        let token_22_nft = "FT6sLdVn6zPYiXeJJSkbrtEw6o2z1mQA74ZMUJA4f77V";
+        let acct = token_22_nft.parse().unwrap();
+        let res = fetch_token_2022_nft_metadata(&client, &acct).await.unwrap();
+        assert!(res.mint == token_22_nft);
     }
 }
